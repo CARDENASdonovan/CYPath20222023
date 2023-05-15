818a9d0bd3437c422f9da6ff587b646b1863ce62
AlexandreJohnson <129733915+AlexandreJohnson@users.noreply.github.com>
2023-05-15 17:00:49 +0200

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,3 +1,11 @@
 public class Shape{
-    
+    int width;
+    int height;
+    String color;
+
+    public Shape(int width, int height, String color){
+        this.height = height;
+        this.width = width;
+        this.color = color;
+    }
 }

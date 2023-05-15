c4cfe39e9dc19aaca8f0933e0bdd8a03a264301f
CARDENASdonovan <129096778+CARDENASdonovan@users.noreply.github.com>
2023-05-15 12:22:50 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class main{
-    public static void main(String args[]){}
-
-    public static void createurs(){
-        System.Out.Println("RUELLAN Baptiste\nCARDENAS Donovan\nJONHSON Alexandre\nGUENNEAU Romain\nMACHNIK Adrien");
-    }
-}

cb8a0636e11b65ff282255be38caf143df02d034
Baat01 <baat01.p@gmail.com>
2023-05-15 11:38:55 +0200

    Update main.java
    add fonction creator()

--- main.java
+++ main.java
@@ -1,3 +1,7 @@
 public class main{
     public static void main(String args[]){}
+
+    public static void createurs(){
+        System.Out.Println("RUELLAN Baptiste\nCARDENAS Donovan\nJONHSON Alexandre\nGUENNEAU Romain\nMACHNIK Adrien");
+    }
 }

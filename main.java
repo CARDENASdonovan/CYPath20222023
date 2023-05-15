0fb352bc0920416be824fdbac64502ecae6f447f
CARDENASdonovan <cardenaste@cy-tech.fr>
2023-05-15 11:55:25 +0200

    ReTest

--- main.java
+++ main.java
@@ -3,6 +3,5 @@ public class main{
 
     public static void createurs(){
         System.Out.Println("RUELLAN Baptiste\nCARDENAS Donovan\nJONHSON Alexandre\nGUENNEAU Romain\nMACHNIK Adrien");
-        System.Out.Println("Feur");
     }
 }

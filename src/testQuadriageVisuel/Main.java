7ab8d605ba147246802770ea64c56fa1eb40e2bf
AlexandreJohnson <129733915+AlexandreJohnson@users.noreply.github.com>
2023-05-17 15:38:15 +0200

    bouton en plus

--- src/testQuadriageVisuel/Main.java
+++ src/testQuadriageVisuel/Main.java
@@ -1,6 +1,10 @@
 package testQuadriageVisuel;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -78,6 +82,7 @@ public class Main extends Application {
 			        root.getChildren().add(rect1);
 			        
 			        Text tileNumber = new Text(20 + debutX + widthCorner + (heightSquare + heightCorner) * acc2, 30 + debutY + heightCorner + (heightSquare + heightCorner) * acc3, Integer.toString(accSquareId));
+			        tileNumber.setId(Integer.toString(accSquareId));
 			        root.getChildren().add(tileNumber);
 			        accSquareId++;
 			        
@@ -107,11 +112,27 @@ public class Main extends Application {
         
         System.out.println(root.getChildren().size());
         for(int i = 0; i < root.getChildren().size(); i++) {
-        	if(root.getChildren().get(i).getClass().getSimpleName() == "Text") {
+        	if(root.getChildren().get(i).getClass().getSimpleName().equals("Text")) {
         		System.out.println(root.getChildren().get(i).getId());
         		System.out.println(root.getChildren().get(i));
         	}
         }
+        Player a = new Player(300);  // Sensé ajouter un cercle mais ne fonctionne pas pour le moment
+        root.getChildren().add(a);
+        
+        Button button = new Button("Poser une barrière"); // Add a button
+        AnchorPane.setTopAnchor(button, 0.0);
+        AnchorPane.setLeftAnchor(button,570.0 );
+        
+        button.setOnAction(new EventHandler<ActionEvent>(){
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("caca");
+        	}
+        	
+        });
+        
+        root.getChildren().add(button);
         
         Scene scene = new Scene(root, 650, 650);
         
